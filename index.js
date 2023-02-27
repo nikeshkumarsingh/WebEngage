f255e8d5c70b7bb273fd6cdcc88d1b218af68a3b
nikeshkumar <nikeshmasai1947@gmail.com>
2023-02-27 15:36:00 +0530

    css update

---
+++ index.js
@@ -0,0 +1,8 @@
+const Form =document.querySelector("form");
+
+Form.addEventListener("submit",(ev)=>{
+    ev.preventDefault();
+    let stat=true;
+    const reg=/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+    
+})
